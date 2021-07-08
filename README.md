e08d8b4df429edcf208e0c41eed71f807758d1de
Albert E <vradnisntlong@gmail.com>
2021-07-08 16:44:32 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,24 @@
+# Stopwatch Using Tkinter in Python #
+
+Stopwatch (what else do you want?)
+
+### Short Explanation ###
+
+The program uses OpenCV module and the haar cascade classifier for face recognition/tracking.
+
+- The OpenCV module is used for getting the video from a capture device.
+- Tkinter is used for making a window GUI.
+- Program is a stopwatch that can count seconds, minutes, and hours.
+
+### Dependencies ###
+
+- Python 3 (program made with ver 3.9.5)
+- Tkinter (version 8.6) - usually is installed with the default Python 3 library
+
+### Program Set Up ###
+
+- Install the required modules.
+- Run main.py.
+
+### Notes ###
+- Time format - hours:minutes:seconds
---
+++ main.py
@@ -0,0 +1,94 @@
+import tkinter as tk
+
+#init
+isRunning = False
+second = 0
+minute = 0
+hour = 0
+resetTime = '0:0:0'
+master = tk.Tk()
+master.title('Stopwatch')
+master.minsize(width = 180, height = 140)
+
+def update(timeText):
+    def logic():
+        if isRunning:
+            global second
+            global minute
+            global hour
+            if second == 0:
+                text = resetTime
+            else:
+                strsecond = str(second)
+                strminute = str(minute)
+                strhour = str(hour)
+                text = strhour + ':' + strminute + ':' + strsecond
+            timeText['text'] = text
+            timeText.after(1000, logic)
+            second += 1
+            if second % 60 == 0 and second != 0:
+                minute += 1
+            if minute % 60 == 0 and minute != 0:
+                hour += 1
+    logic()
+
+def Start(timeText):
+    global isRunning
+    isRunning = True
+    update(timeText)
+    start['state'] = 'disabled'
+    stop['state'] = 'normal'
+    reset['state'] = 'normal'
+
+def Stop():
+    global isRunning
+    isRunning = False
+    start['state'] = 'normal'
+    stop['state'] = 'disabled'
+    reset['state'] = 'normal'
+
+def Reset(timeText):
+    global second
+    second = 0
+    if (isRunning == False):
+        reset['state'] = 'disabled'
+        timeText['text'] = resetTime
+    else:
+        timeText['text'] = resetTime
+
+#window content
+title = tk.Label(master,
+            text = 'Stopwatch',
+            font = ('Arial', 15)
+            )
+timeText = tk.Label(master,
+            text = resetTime,
+            font = ('Arial', 20)
+            )
+start = tk.Button(master,
+            text = 'Start',
+            width = 5,
+            font = ('Arial' , 10),
+            command = lambda: Start(timeText)
+            )
+stop = tk.Button(master,
+            text = 'Stop',
+            width = 5,
+            font = ('Arial', 10),
+            command = Stop
+            )
+reset = tk.Button(master,
+            text = 'Reset',
+            width = 5,
+            font = ('Arial', 10),
+            command = lambda: Reset(timeText)
+            )
+
+#placing the window contents
+title.grid(row = 0, column = 0, pady = 1, columnspan = 3)
+timeText.grid(row = 1, column = 0, pady = 1, padx = 20, rowspan = 3, columnspan = 2)
+start.grid(row = 1, column = 2, pady = 1)
+stop.grid(row = 2, column = 2, pady = 1)
+reset.grid(row = 3, column = 2, pady = 1)
+
+master.mainloop()
